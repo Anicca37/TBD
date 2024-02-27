4a8ea462d894eb0a3da13175d91fabe48f3823a5
Anicca37 <sapphire.li2001@gmail.com>
2024-02-26 20:32:35 -0500

    decrease camera sensitivity

--- Assets/Scripts/player/fpsCameraControl.cs
+++ Assets/Scripts/player/fpsCameraControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class fpsCameraControl : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 70f;
 
     public Transform player;
 
