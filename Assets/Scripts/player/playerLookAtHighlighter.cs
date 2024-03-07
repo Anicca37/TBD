0445d95fd182c147eb4fd12b286e4b8bc3e79aa4
Googlingprogrammer <94053049+Googlingprogrammer@users.noreply.github.com>
2024-03-07 04:27:43 -0500

    Minor bugfix
    Made sure interactable model had read/write enabled

--- Assets/Scripts/player/playerLookAtHighlighter.cs
+++ Assets/Scripts/player/playerLookAtHighlighter.cs
@@ -16,25 +16,22 @@ public class playerLookAtHighlighter : MonoBehaviour
             // Create a ray from the camera's position and forward direction
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
 
-            // Declare a RaycastHit variable to store information about the hit
             RaycastHit hit;
 
             // Check if the ray hits an object within the specified distance
             if (Physics.Raycast(ray, out hit, raycastDistance))
             {
-                // Check if the hit object has a specific tag (you can customize this condition)
+                // Check if the hit object has a specific tag
                 if (hit.collider.CompareTag("Pickupable") || hit.collider.CompareTag("Interactable"))
                 {
-                    // Player is looking at the object
-                    // Debug.Log("Player is looking at the object!");
 
-                    // Try to get the "Testbug" script on the hit object
+                    // Try to get the "Outline" script on the hit object
                     outlineScript = hit.collider.GetComponent<Outline>();
 
                     // Check if the script is found
                     if (outlineScript != null)
                     {
-                        // Enable the "Testbug" script on the object
+                        // Enable the "Outline" script on the object
                         outlineScript.enabled = true;
                     }
                 }
