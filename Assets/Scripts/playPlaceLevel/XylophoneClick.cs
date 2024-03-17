d205bc84fad9f1426f0421ae0231437c26605fc0
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2024-03-17 19:02:08 -0400

    reimplement ball :') & add ketchup model

--- Assets/Scripts/playPlaceLevel/XylophoneClick.cs
+++ Assets/Scripts/playPlaceLevel/XylophoneClick.cs
@@ -9,6 +9,6 @@ public class XylophoneClick : MonoBehaviour
     private void OnMouseDown()
     {
         FindObjectOfType<XSequenceChecker>().XyloClicked(xyloID);
-        PlayPlaceManager.Instance.CompletePuzzle("Xylophone");
+        // PlayPlaceManager.Instance.CompletePuzzle("Xylophone");
     }
 }
