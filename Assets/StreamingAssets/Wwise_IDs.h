d1aa7b4c0e9ead281e3afc894badcad949fd9124
NotValidSky <ketrolus@gmail.com>
2024-03-18 12:09:08 -0400

    soundbank regen

--- Assets/StreamingAssets/Wwise_IDs.h
+++ Assets/StreamingAssets/Wwise_IDs.h
@@ -79,7 +79,10 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
