c3ad2e5ef2e7c504179d6e442d45e13f3ca14d5d
NotValidSky <ketrolus@gmail.com>
2024-03-19 12:33:12 -0400

    more sound fix

--- Assets/StreamingAssets/Wwise_IDs.h
+++ Assets/StreamingAssets/Wwise_IDs.h
@@ -58,7 +58,7 @@ namespace AK
         static const AkUniqueID PLAY_XYLOD = 4153846228U;
         static const AkUniqueID PLAY_XYLOE = 4153846229U;
         static const AkUniqueID PLAY_XYLOG = 4153846231U;
-        static const AkUniqueID PLAY_XYLOSEQUENCE_FIXED = 3187775344U;
+        static const AkUniqueID PLAY_XYLOSEQUENCE = 4293423455U;
         static const AkUniqueID STOP_CHIME_MELODY = 184097815U;
         static const AkUniqueID STOP_CLOCK_TICK = 2897827502U;
         static const AkUniqueID STOP_CLOCK_TICK_REVERSE = 3434769657U;
