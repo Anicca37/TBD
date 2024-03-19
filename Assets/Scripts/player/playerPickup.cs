40f5a759b80e8e52d747cb5427e97aa7f69809a0
Inaas Asad <inaas_asad@hotmail.com>
2024-03-18 20:25:04 -0400

    Added colliders to buckets

--- Assets/Scripts/player/playerPickup.cs
+++ Assets/Scripts/player/playerPickup.cs
@@ -36,38 +36,56 @@ public class playerPickup : MonoBehaviour
     void PickUpObject()
     {
         // Raycast to detect pickupable objects
+         Camera playerCamera = Camera.main; // Get the main camera, assuming the player is using the main camera
+
+        // Raycast from the camera, not from the player's position
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, pickupRange))
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition); // Cast the ray from where the mouse pointer is on the screen
+
+        if (Physics.Raycast(ray, out hit, pickupRange * 3))
         {
             if (hit.collider.CompareTag(pickupableTag))
             {
-                // Set the current pickup to the hit object
-                currentPickup = hit.collider.gameObject;
+                // Calculate the horizontal distance to the hit object, ignoring the Y component
+                Vector3 horizontalPlayerPosition = new Vector3(transform.position.x, 0f, transform.position.z);
+                Vector3 hitPointHorizontal = new Vector3(hit.point.x, 0f, hit.point.z);
+                float horizontalDistance = Vector3.Distance(horizontalPlayerPosition, hitPointHorizontal);
 
-                //disable physics
-                currentPickupRb = currentPickup.GetComponent<Rigidbody>();
-                if (currentPickupRb != null)
-                {
-                    SwitchIcon(true);
-                    currentPickupRb.isKinematic = true;
-                }
+                // Check if the hit object is within the allowable Y distance
+                float yDistance = Mathf.Abs(hit.point.y - transform.position.y);
+                float playerHeight = 2f; // Arbitrary player height, adjust as necessary
 
-                // Attach the pickup to the attachPoint
-                currentPickup.transform.parent = attachPoint;
-                // currentPickup.transform.localPosition = Vector3.zero;
-                // currentPickup.transform.localRotation = Quaternion.identity;
+                // Check if the object is within pickup range horizontally and vertically
+                if (horizontalDistance <= pickupRange && yDistance <= playerHeight)
+                {
+                    // Set the current pickup to the hit object
+                    currentPickup = hit.collider.gameObject;
 
-                if (currentPickup.name.Contains("flower"))
-                {
-                    AkSoundEngine.PostEvent("Play_FlowerPickUp", this.gameObject);
-                }
-                else if (currentPickup.name.Contains("Chair"))
-                {
-                    AkSoundEngine.PostEvent("Play_TablePickUp", this.gameObject);
-                }
-                else if (currentPickup.name.Contains("pinecone"))
-                {
-                    AkSoundEngine.PostEvent("Play_PineconePickup", this.gameObject);
+                    //disable physics
+                    currentPickupRb = currentPickup.GetComponent<Rigidbody>();
+                    if (currentPickupRb != null)
+                    {
+                        SwitchIcon(true);
+                        currentPickupRb.isKinematic = true;
+                    }
+
+                    // Attach the pickup to the attachPoint
+                    currentPickup.transform.parent = attachPoint;
+                    // currentPickup.transform.localPosition = Vector3.zero;
+                    // currentPickup.transform.localRotation = Quaternion.identity;
+
+                    if (currentPickup.name.Contains("flower"))
+                    {
+                        AkSoundEngine.PostEvent("Play_FlowerPickUp", this.gameObject);
+                    }
+                    else if (currentPickup.name.Contains("Chair"))
+                    {
+                        AkSoundEngine.PostEvent("Play_TablePickUp", this.gameObject);
+                    }
+                    else if (currentPickup.name.Contains("pinecone"))
+                    {
+                        AkSoundEngine.PostEvent("Play_PineconePickup", this.gameObject);
+                    }
                 }
             }
         }
