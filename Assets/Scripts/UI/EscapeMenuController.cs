d29590083cc8309b36c88273960ffe2e0ccabead
Anicca37 <sapphire.li2001@gmail.com>
2024-03-24 00:56:45 -0400

    update escape

--- Assets/Scripts/UI/EscapeMenuController.cs
+++ Assets/Scripts/UI/EscapeMenuController.cs
@@ -50,15 +50,15 @@ public class EscapeMenuController : MonoBehaviour
         {
             playerBody.GetComponent<playerMovement>().enabled = false;
             // handle input
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (InputManager.instance.SelectionUpInput)
             {
                 MoveSelectionUp();
             }
-            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            else if (InputManager.instance.SelectionDownInput)
             {
                 MoveSelectionDown();
             }
-            else if (Input.GetKeyDown(KeyCode.Return))
+            else if (InputManager.instance.ConfirmInput)
             {
                 SelectOption();
             }
