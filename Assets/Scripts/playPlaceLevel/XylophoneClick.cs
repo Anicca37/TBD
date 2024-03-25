e51928b2c7371d047313cd360b830ac68224040d
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2024-03-24 22:47:51 -0400

    implement click animation

--- Assets/Scripts/playPlaceLevel/XylophoneClick.cs
+++ Assets/Scripts/playPlaceLevel/XylophoneClick.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class XylophoneClick : MonoBehaviour, IInteract
 {
     public int xyloID;
+    [SerializeField] private Animator xyloAnimator;
 
-    public XSequenceChecker XsequenceChecker;
+    // public XSequenceChecker XsequenceChecker;
 
     public void OnMouseDown()
     {
         FindObjectOfType<XSequenceChecker>().XyloClicked(xyloID);
+        xyloAnimator.SetTrigger($"Hit {xyloID}"); // animate hit
         PlayXyloSound(xyloID);
+        StartCoroutine(ResetAnimation(xyloID, 1f));
     }
 
     void PlayXyloSound(int xyloID)
@@ -20,7 +23,7 @@ public class XylophoneClick : MonoBehaviour, IInteract
         switch (xyloID)
         {
             case 1:
-                AkSoundEngine.PostEvent("Play_XyloC", this.gameObject);        
+                AkSoundEngine.PostEvent("Play_XyloC", this.gameObject);
                 break;
             case 2:
                 AkSoundEngine.PostEvent("Play_XyloD", this.gameObject);
@@ -32,5 +35,10 @@ public class XylophoneClick : MonoBehaviour, IInteract
                 AkSoundEngine.PostEvent("Play_XyloG", this.gameObject);
                 break;
         }
+    }
+    IEnumerator ResetAnimation(int id, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        xyloAnimator.SetTrigger($"Return {id}");
     }
 }
