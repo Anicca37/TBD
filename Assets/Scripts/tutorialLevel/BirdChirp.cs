31d0dd0e147ecf51114a0822c0b096ee5351f6d1
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2024-03-26 23:54:28 -0400

    add bird chirp script

---
+++ Assets/Scripts/tutorialLevel/BirdChirp.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdChirp : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Chirp!");
+        // add chirp sound
+    }
+}
