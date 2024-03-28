e7527e2c8e43abdb9b1362847e1275db7e9fabc9
NotValidSky <ketrolus@gmail.com>
2024-03-27 22:06:40 -0400

    tut bird only play once

--- Assets/Scripts/tutorialLevel/BirdWindow.cs
+++ Assets/Scripts/tutorialLevel/BirdWindow.cs
@@ -6,14 +6,21 @@ public class BirdWindow : MonoBehaviour
 {
     [SerializeField] private Animator bird;
 
+    private bool soundPlayed = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Chirp");
             bird.SetTrigger("Warn");
-            Invoke("playBirdWing", 1.4f);
-            Invoke("ResetBird", 4.5f);
+
+            if (soundPlayed == false)
+            {
+                Invoke("playBirdWing", 1.4f);
+                Invoke("ResetBird", 4.5f);
+                soundPlayed = true;
+            }
         }
     }
     private void ResetBird()
